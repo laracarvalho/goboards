d8dd84d25279fd095db5762d7b56128be915c47f
Lara Carvalho <laraftoller@gmail.com>
2023-06-17 17:03:45 -0300

    test: add tests for all handlers

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -38,3 +38,63 @@ func TestCreateListingHandler(t *testing.T) {
 		assert.Equal(t, 400, w.Code)
 	}
 }
+
+func TestUpdateListingHandler(t *testing.T) {
+	r := router.Start()
+
+	w := httptest.NewRecorder()
+
+	tests := []struct {
+		payload []byte
+	}{
+		{[]byte("")},
+	}
+
+	for _, tt := range tests {
+		body := bytes.NewReader(tt.payload)
+		req, _ := http.NewRequest("PUT", "/api/v1/listing", body)
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, 400, w.Code)
+	}
+}
+
+func TestDeleteListingHandler(t *testing.T) {
+	r := router.Start()
+
+	w := httptest.NewRecorder()
+
+	tests := []struct {
+		payload []byte
+	}{
+		{[]byte("")},
+	}
+
+	for _, tt := range tests {
+		body := bytes.NewReader(tt.payload)
+		req, _ := http.NewRequest("DELETE", "/api/v1/listing", body)
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, 400, w.Code)
+	}
+}
+
+func TestShowListingHandler(t *testing.T) {
+	r := router.Start()
+
+	w := httptest.NewRecorder()
+
+	tests := []struct {
+		payload []byte
+	}{
+		{[]byte("")},
+	}
+
+	for _, tt := range tests {
+		body := bytes.NewReader(tt.payload)
+		req, _ := http.NewRequest("GET", "/api/v1/listing?id=", body)
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, 400, w.Code)
+	}
+}
